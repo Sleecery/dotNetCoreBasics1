560dff29b9e5c58e97eba4ca68aa21330f3ae1a1
Filip Konecny <fkonecny2@gmail.com>
2018-06-04 00:31:29 +0200

    TODO: upload files

---
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Models.MovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync(string movieGenre, string searchString)
+        {
+            var genreQuery = _context.Movie.OrderBy(x => x.Genre).
+                Select(x => x.Genre);
+
+            var movies = _context.Movie.Select(x => x);
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(x => x.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
